ad556fd66bf76a32bc4ce3761addbdf84f08ebc8
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-04-29 12:29:16 -0400

    adding content

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
+function classToggle() {
+    const navs = document.querySelectorAll('.navbar-items')
+
+    navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+}
+
+document.querySelector('.navbar-link-toggle')
+    .addEventListener('click', classToggle);
+
+
+
+
 //Sticky Menu background
 window.addEventListener('scroll', function () {
     if (window.scrollY > 150) {
-        document.querySelector('nav').style.backgroundColor = "#363636";
+        document.querySelector('.navbar').style.backgroundColor = "#363636";
     } else if (window.scrollY > 50) {
-        document.querySelector('nav').style.backgroundColor = "transparent";
+        document.querySelector('.navbar').style.backgroundColor = "transparent";
     }
 });
