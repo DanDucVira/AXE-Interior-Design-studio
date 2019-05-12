5398203c442050ad3745a57be23b8ddec776d268
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-05-11 21:12:09 -0400

    added smooth scroll to webpage

--- js/main.js
+++ js/main.js
@@ -18,3 +18,15 @@ window.addEventListener('scroll', function () {
         document.querySelector('.navbar').style.backgroundColor = "transparent";
     }
 });
+//Smooth Scrolling
+$('.navbar a').on('click', function (event) {
+    if (this.hash !== '') {
+        event.preventDefault();
+        const hash = this.hash;
+        $('html, body').animate({
+                scrollTop: $(hash).offset().top - 100
+            },
+            800
+        );
+    }
+});
